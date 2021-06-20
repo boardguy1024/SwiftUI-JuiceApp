182d6ac300de8d78272488e4f548fd35a7fe12af
boardguy1024 <boardguy1024@gmail.com>
2021-06-20 20:54:48 +0900

    Initial Commit

---
+++ FoodAppWithSwiftUI/FoodAppWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppWithSwiftUIApp.swift
+//  FoodAppWithSwiftUI
+//
+//  Created by park kyung seok on 2021/06/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
